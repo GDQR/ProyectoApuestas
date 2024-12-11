336987610cd462a84505446eabb6cc48e1436aa9
Guido Diego Quispe Robles <quispeguidodr@gmail.com>
2024-12-11 16:48:50 -0300

    Update ResultadoDAO.java

--- src/main/java/com/mycompany/apuestatodook/model/ResultadoDAO.java
+++ src/main/java/com/mycompany/apuestatodook/model/ResultadoDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import org.hibernate.Session;
 
 public class ResultadoDAO {
     private List<Resultado> resultados;
@@ -21,52 +22,87 @@ public class ResultadoDAO {
 
     public List<Resultado> getAllResultados() {
         List<Resultado> resultados = new LinkedList<>();
-        String query = "SELECT * FROM resultado";
-        try (Connection con = ConnectionPool.getInstance().getConnection();
-             PreparedStatement preparedStatement = con.prepareStatement(query);
-             ResultSet resultSet = preparedStatement.executeQuery()) {
-            while (resultSet.next()) {
-                resultados.add(rsRowToResultado(resultSet));
-            }
-        } catch (SQLException ex) {
+        try(Session session = HibernateUtil.getSessionFactory().getCurrentSession() ){
+            session.beginTransaction();
+            resultados = session.createQuery("FROM Resultado", Resultado.class)
+                    .getResultList();
+            session.getTransaction().commit();
+            
+        } catch(Exception ex){
             throw new RuntimeException(ex);
         }
         return resultados;
+//        List<Resultado> resultados = new LinkedList<>();
+//        String query = "SELECT * FROM resultado";
+//        try (Connection con = ConnectionPool.getInstance().getConnection();
+//             PreparedStatement preparedStatement = con.prepareStatement(query);
+//             ResultSet resultSet = preparedStatement.executeQuery()) {
+//            while (resultSet.next()) {
+//                resultados.add(rsRowToResultado(resultSet));
+//            }
+//        } catch (SQLException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//        return resultados;
     }
 
     public Resultado getResultadoByIdPartido(int idPartido) {
-        String query = "SELECT * FROM resultado WHERE fk_id_partido = ?";
         Resultado resultado = null;
-        try (Connection con = ConnectionPool.getInstance().getConnection();
-             PreparedStatement preparedStatement = con.prepareStatement(query)) {
-            preparedStatement.setInt(1, idPartido);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    resultado = rsRowToResultado(resultSet);
-                }
-            }
-        } catch (SQLException ex) {
+        try(Session session = HibernateUtil.getSessionFactory().getCurrentSession() ){
+            session.beginTransaction();
+            resultado = session.createQuery("FROM Resultado WHERE fk_id_partido = ?1", Resultado.class)
+                    .setParameter(1, idPartido)
+                    .getSingleResultOrNull();
+            session.getTransaction().commit();
+            
+        } catch(Exception ex){
             throw new RuntimeException(ex);
         }
-        
         return resultado;
+//        String query = "SELECT * FROM resultado WHERE fk_id_partido = ?";
+//        Resultado resultado = null;
+//        try (Connection con = ConnectionPool.getInstance().getConnection();
+//             PreparedStatement preparedStatement = con.prepareStatement(query)) {
+//            preparedStatement.setInt(1, idPartido);
+//            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+//                if (resultSet.next()) {
+//                    resultado = rsRowToResultado(resultSet);
+//                }
+//            }
+//        } catch (SQLException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//        
+//        return resultado;
     }
     
      public int getIdResultadoByIdPartido(int idPartido) {
-        String query = "SELECT id_resultado FROM resultado WHERE fk_id_partido = ?";
-        try (Connection con = ConnectionPool.getInstance().getConnection();
-             PreparedStatement preparedStatement = con.prepareStatement(query)) {
-            preparedStatement.setInt(1, idPartido);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    return resultSet.getInt("id_resultado");
-                } else {
-                    throw new RuntimeException("No se encontró ningún resultado para el partido con id " + idPartido);
-                }
-            }
-        } catch (SQLException ex) {
+        try(Session session = HibernateUtil.getSessionFactory().getCurrentSession() ){
+            session.beginTransaction();
+            int id = session.createQuery("SELECT idResultado FROM Resultado WHERE fk_id_partido = ?1", int.class)
+                    .setParameter(1, idPartido)
+                    .getSingleResult();
+            
+            session.getTransaction().commit();
+            
+            return id;
+        } catch(Exception ex){
             throw new RuntimeException(ex);
         }
+//        String query = "SELECT id_resultado FROM resultado WHERE fk_id_partido = ?";
+//        try (Connection con = ConnectionPool.getInstance().getConnection();
+//             PreparedStatement preparedStatement = con.prepareStatement(query)) {
+//            preparedStatement.setInt(1, idPartido);
+//            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+//                if (resultSet.next()) {
+//                    return resultSet.getInt("id_resultado");
+//                } else {
+//                    throw new RuntimeException("No se encontró ningún resultado para el partido con id " + idPartido);
+//                }
+//            }
+//        } catch (SQLException ex) {
+//            throw new RuntimeException(ex);
+//        }
     }
 
     private Resultado rsRowToResultado(ResultSet rs) throws SQLException {
